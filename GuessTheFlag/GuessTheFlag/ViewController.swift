05505adcd0535ab055d829cd80a1183ee68a0099
justine wright <wright.justine21@gmail.com>
2021-05-19 21:43:20 +0200

    edited gui

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -14,11 +14,13 @@ class ViewController: UIViewController {
     private lazy var countries = [String]() // lazy = delay instantiation until required
     private lazy var score = 0
     private lazy var correctAnswer = 0
-    
+    private var maxQuestionCount = 10
+    private var currentQuestionCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCountries()
         applyStyleToButtons()
+        allDeselect()
         askQuestion()
     }
     
@@ -44,34 +46,44 @@ class ViewController: UIViewController {
         for button in buttons{
             button.layer.borderWidth = 1
             button.layer.borderColor = UIColor.lightGray.cgColor
+            button.layer.cornerRadius = 5
+            button.tintColor = UIColor.white
+            button.layer.masksToBounds = true
         }
         
         // UIColor (red:, green:, blue:).cgColour
         
     }
-    private func allDeselect(){}
+    private func allDeselect(){
+        for button in buttons{
+            button.alpha = 0.75
+        }
+    }
     // UIAlertAction needed for alertController
     private func askQuestion(action: UIAlertAction! = nil)->Void{
         chooseThreeRandomCountries()
         //for: .normal -> takes in parameter for state of button
         // .normal -> from a struct
-        correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        correctAnswer = Int.random(in: 0...3)
+        title = "\(countries[correctAnswer].uppercased()) [\(score)]"
+    }
+    
+    private func updateQuestionCount(){
+        currentQuestionCount += 1
     }
     private func chooseThreeRandomCountries(){
         countries.shuffle()
-        let countriesRange = (0...countries.count-1).shuffled()
         var index = 0
         for button in buttons{
-            let name = countries[countriesRange[index]]
+            let name = countries[index]
             button.setImage(UIImage(named: name), for: .normal)
             index += 1
         }
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Expected Tag: \(correctAnswer) --- Actual Tag \(sender.tag)")
         score = sender.tag == correctAnswer ? score + 1: score - 1
-        let title = sender.tag == correctAnswer ? "Correct": "Wrong"
-        showAlert(title: title)
+        showAlert(title: sender.tag == correctAnswer ? "Correct": "Wrong")
     }
     
     private func showAlert(title:String){
@@ -80,6 +92,9 @@ class ViewController: UIViewController {
         
         present(alertController, animated: true)
 
+    }
+    func updateProgressBar(){
+        
     }
     //ca layer = core animation layer
     
@@ -94,7 +109,7 @@ class ViewController: UIViewController {
     // 3. pull request
     
     // 2 Challenge
-    // -show score in navigation bar
+    // + show score in navigation bar
     // -keep track of how many questions have been asked
     // -max queestions 10
     // -10 -> final alert controller
