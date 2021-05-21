375a9b7970bfe5cef9b0781ab60d48945220e37a
justine wright <wright.justine21@gmail.com>
2021-05-21 13:29:54 +0200

    refactor: added guards and computed variabled

--- GuessTheFlag/GuessTheFlag/Controller/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/Controller/ViewController.swift
@@ -14,41 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var countryText: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var progressText: UILabel!
-    var quizBrain = QuizBrain()
-//    private lazy var countries = [String]() // lazy = delay instantiation until required
-//    private lazy var score = 0
-//    private lazy var correctAnswer = 0
-//    private var maxQuestionCount = 10
-//    private var currentQuestionCount = 0
+    private lazy var quizBrain = QuizBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        //updateUI
         applyStyleToButtons()
         updateUI()
     }
 
-    private func applyStyleToButtons(){
-        for button in buttons{
+    private func applyStyleToButtons() {
+        buttons.forEach{button in
             button.layer.borderWidth = 1
             button.layer.borderColor = UIColor.lightGray.cgColor
             button.layer.cornerRadius = 5
             button.tintColor = UIColor.white
             button.layer.masksToBounds = true
         }
-        
-        // UIColor (red:, green:, blue:).cgColour
-        
     }
     
-    private func allDeselect(){
-        for button in buttons{
+    private func allDeselect() {
+        buttons.forEach{button in
             button.layer.borderColor = UIColor.lightGray.cgColor
             button.layer.borderWidth = 1
             button.alpha = 0.75
         }
     }
     
-    private func updateUI(){
+    private func updateUI() {
         setScoreText()
         allDeselect()
         updateProgressBar()
@@ -56,23 +48,20 @@ class ViewController: UIViewController {
         setCorrectCountryText()
     }
     
-    // UIAlertAction needed for alertController
-    private func askQuestion(action: UIAlertAction! = nil)->Void{
-        //for: .normal -> takes in parameter for state of button
-        // .normal -> from a struct
+    private func askQuestion(action: UIAlertAction! = nil) -> Void {
         quizBrain.nextQuestion()
         updateUI()
     }
     
-    private func setCorrectCountryText(){
-        countryText.text = "\(quizBrain.getCorrectAnswerText())"
+    private func setCorrectCountryText() {
+        countryText.text = "\(quizBrain.correctAnswerText)"
     }
     
     private func setScoreText(){
-        title = "Your Score: \(quizBrain.getScore())/10"
+        title = "Your Score: \(quizBrain.score)/10"
     }
     
-    private func setButtonImages(){
+    private func setButtonImages() {
         var index = 0
         for button in buttons{
             let name = quizBrain.getCountryAtIndex(index: index)
@@ -83,102 +72,63 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         let correctAnswer = quizBrain.checkAnswer(answer: sender.tag)
-        highlightRightFlag()
+        highlightFlag(at: quizBrain.correctAnswerIndex, withColour: UIColor.green)
         if correctAnswer {
             makeRightAnswerAlert()
         }
         else{
-            highlightBadFlag(actualAnswer: sender.tag)
-            makeWrongAnswerAlert(actualAnswer: sender.tag)
+            highlightFlag(at: sender.tag, withColour: UIColor.red)
+            makeWrongAnswerAlert(userAnswer: sender.tag)
         }
     }
     
-    private func makeRightAnswerAlert(){
+    private func makeRightAnswerAlert() {
         let title = "Correct"
-        let msg = ["Good job! :)", "Fabulous! :)", "Amazing! :)"].randomElement()!
+        let msg = ["Good job! 😇", "Fabulous! 🥸", "Amazing! 😎"].randomElement()!
         showAlert(title: title, message:  msg)
     }
     
-    private func makeWrongAnswerAlert(actualAnswer: Int){
-        highlightBadFlag(actualAnswer: actualAnswer)
+    private func makeWrongAnswerAlert(userAnswer: Int) {
         let title = "Wrong"
-        let msg = "You guessed \(quizBrain.getCountryAtIndex(index: actualAnswer)) :("
+        let msg = "You guessed \(quizBrain.getCountryAtIndex(index: userAnswer)) 🥺"
         showAlert(title: title, message:  msg)
     }
  
-    private func showAlert(title:String, message: String = "Good job!"){
-        if quizBrain.isGameRunning(){
-            showQuestionFeedbackAlert(title: title, message: message)
-        }else{
-            showFinalAlert(title: title)
+    private func showAlert(title: String, message: String) {
+        guard quizBrain.isGameRunning else {
+            showQuestionFeedbackAlert2(title: title, message: message, actionTitle: "Restart", handler: restart)
+            maxProgressBar()
+            return
         }
+        showQuestionFeedbackAlert2(title: title, message: message, actionTitle: "Continue", handler: askQuestion)
     }
     
-    private func showQuestionFeedbackAlert(title:String, message: String){
+    private func showQuestionFeedbackAlert2(title: String, message: String, actionTitle: String, handler: ((UIAlertAction) -> Void)?) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        alertController.addAction(UIAlertAction(title: actionTitle, style: .default, handler: handler))
         present(alertController, animated: true)
     }
     
-    private func showFinalAlert(title:String ){
-        let alertController = UIAlertController(title: title, message: "Your final score is \(quizBrain.getScore())/10", preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Quit", style: .default, handler: quit))
-        
-        present(alertController, animated: true)
+    private func highlightFlag(at index: Int, withColour color: UIColor) {
+        buttons[index].layer.borderColor = color.cgColor
+        buttons[index].alpha = 1
+        buttons[index].layer.borderWidth = 5
     }
     
-    private func highlightBadFlag(actualAnswer:Int){
-        buttons[actualAnswer].layer.borderColor = UIColor.red.cgColor
-        buttons[actualAnswer].alpha = 1
-        buttons[actualAnswer].layer.borderWidth = 5
-        
-    }
-    
-    private func highlightRightFlag(){
-        let correctAnswerIndex = quizBrain.getCorrectAnswerIndex()
-        buttons[correctAnswerIndex].layer.borderColor = UIColor.green.cgColor
-        buttons[correctAnswerIndex].alpha = 1
-        buttons[correctAnswerIndex].layer.borderWidth = 5
-    }
-    
-    private func quit(action: UIAlertAction! = nil){
-        exit(0)
+    private func restart(action: UIAlertAction! = nil) {
+        quizBrain.reset()
+        updateUI()
     }
 
-    private func updateProgressBar(){
-        let percentage = quizBrain.getProgress()
-        updateProgressBar(percentage: percentage)
-        updateProgressText(percentage: percentage)
-    }
-    
-    private func updateProgressBar(percentage: Float){
+    private func updateProgressBar() {
+        let percentage = quizBrain.progress
         progressBar.progress = percentage
+        progressText.text = "\(Int( round(percentage*100.00) ))%"
     }
     
-    private func updateProgressText(percentage: Float){
-        progressText.text = "\(Int(round(percentage*100.00) ))%"
+    private func maxProgressBar(){
+        progressBar.progress = 1
+        progressText.text = "\(100)%"
     }
-    
-    //ca layer = core animation layer
-    
-    //Layer order:
-    // uiview uikit
-    // uicolor
-    // ca cg
-    
-    // 1 Challenges
-    // 1. refactor code
-    // 2. add this to github
-    // 3. pull request
-    
-    // 2 Challenge
-    // + show score in navigation bar
-    // + keep track of how many questions have been asked
-    // +max queestions 10
-    // +10 -> final alert controller
-    
-    // 3 challenge
-    // +mistake
-    // +tell them what flag they selected, and what flag was right
 }
 
--- GuessTheFlag/GuessTheFlag/Model/QuizBrain.swift
+++ GuessTheFlag/GuessTheFlag/Model/QuizBrain.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct QuizBrain{
+class QuizBrain{
     private var countries = [
         "estonia",
         "france",
@@ -24,7 +24,7 @@ struct QuizBrain{
     ]
     private var maxQuestionCount = 10
     private var questionNumber = 0
-    private var score = 0
+    private(set) var score = 0
     private lazy var correctAnswer = 0
 
     
@@ -33,26 +33,21 @@ struct QuizBrain{
         questionNumber = 0
     }
     
-    mutating func nextQuestion(){
+    func nextQuestion() {
         questionNumber += 1
         correctAnswer = Int.random(in: 0...3)
         countries.shuffle()
     }
     
-    mutating func getCountryChoices()-> [String]{
-        let chosenCountries = countries[...3]
-        return Array(chosenCountries)
-    }
-    
-    mutating func getCorrectAnswerText()->String{
+    var correctAnswerText: String {
         return countries[correctAnswer].uppercased()
     }
     
-    mutating func getCorrectAnswerIndex()->Int{
-        return correctAnswer
+    var correctAnswerIndex: Int {
+        correctAnswer
     }
     
-    mutating func checkAnswer(answer:Int) -> Bool{
+    func checkAnswer(answer:Int) -> Bool {
         let isCorrect = countries[correctAnswer] == countries[answer]
         score += isCorrect ? 1 : -1
         return isCorrect
@@ -62,15 +57,17 @@ struct QuizBrain{
         return countries[index]
     }
     
-    func getProgress()->Float{
-        return Float(questionNumber+1)/Float(maxQuestionCount)
+    var progress: Float {
+        return Float(questionNumber)/Float(maxQuestionCount)
     }
     
-    func getScore()->Int{
-        return score
-    }
-    
-    func isGameRunning()->Bool{
+    var isGameRunning: Bool {
         return questionNumber+1 < maxQuestionCount
+    }
+    
+    func reset() {
+        nextQuestion()
+        score = 0
+        questionNumber = 0
     }
 }
